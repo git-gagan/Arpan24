48b6bec356798d9548d160458a19f224312f5954
Gaganrajdeep Singh <gaganrajdeepsingh009@gmail.com>
2024-10-31 20:49:03 +0530

    reverted to earlier working config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({
-    output: 'hybrid',
-    adapter: node({
-        mode: 'hybrid' // standalone mode bundles everything for local or server use
-      }),
-});
+// export default defineConfig({
+//     output: 'hybrid',
+//     adapter: node({
+//         mode: 'hybrid' // standalone mode bundles everything for local or server use
+//       }),
+// });
 
-// export default defineConfig({});
+export default defineConfig({});
